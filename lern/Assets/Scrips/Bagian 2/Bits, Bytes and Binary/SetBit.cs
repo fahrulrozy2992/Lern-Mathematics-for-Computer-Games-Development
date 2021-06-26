339e2bea0dd4f127babc1f87e5106867b8b580b1
fahrulrozy2992 <fahrulrozy2992@gmail.com>
2021-06-27 06:36:18 +0700

    Bits, Bytes and Binary

---
+++ lern/Assets/Scrips/Bagian 2/Bits, Bytes and Binary/SetBit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SetBit : MonoBehaviour
+{
+	int bSquence = 8 + 1 + 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    Debug.Log(Convert.ToString(bSquence,2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
