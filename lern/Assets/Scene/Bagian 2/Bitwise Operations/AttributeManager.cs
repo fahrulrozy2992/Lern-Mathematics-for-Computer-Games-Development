c825fdc6baca416fdd9af87e8e71b81478ff48d7
fahrulrozy2992 <fahrulrozy2992@gmail.com>
2021-07-06 09:49:29 +0700

    Bit Flags & Bitwise Oprations

---
+++ lern/Assets/Scene/Bagian 2/Bitwise Operations/AttributeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class AttributeManager : MonoBehaviour
+{
+	static public int MAGIC = 16;
+	public Text attributeDisplay;
+	int attributes =0;
+	// Start is called before the first frame update
+	private void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag =="MAGIC");
+		{
+			attributes = MAGIC;
+		}
+	}
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+	    attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+	    attributeDisplay.text = Convert.ToString(attributes,2).PadLeft(8,'0');
+    }
+       
+}
